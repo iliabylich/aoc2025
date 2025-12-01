13955ffbad1ae6b0d61d6291019c739b8992e9a0
Ilya Bylich <ibylich@gmail.com>
2025-12-01 10:25:21 +0100

    day 1 part 1

---
+++ src/bin/day1/part1.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let input = include_str!("input1");
+
+    println!("{}", solve(input))
+}
+
+fn solve(input: &str) -> u32 {
+    let mut acc = 50;
+    let mut out = 0;
+
+    for line in input.lines() {
+        // let line = line.as_bytes();
+        let mul = match line.as_bytes()[0] {
+            b'L' => -1,
+            b'R' => 1,
+            _ => panic!("wrong direction"),
+        };
+        let num: i32 = line[1..].parse().unwrap();
+
+        acc += mul * num;
+
+        if acc % 100 == 0 {
+            out += 1;
+        }
+    }
+
+    out
+}
+
+#[test]
+fn test() {
+    let input = include_str!("input0");
+    let output = solve(input);
+    assert_eq!(output, 3);
+}
