3b6be008c0b8edb6d56a5708f733dd7abbe0e3a5
Ilya Bylich <ibylich@gmail.com>
2025-12-04 10:26:20 +0100

    day 4 part 1

---
+++ src/bin/day4/part1.rs
@@ -0,0 +1,103 @@
+fn main() {
+    println!("{}", solve(include_str!("input1")))
+}
+
+struct Matrix {
+    nrows: usize,
+    ncols: usize,
+    data: Vec<Vec<u8>>,
+}
+
+impl Matrix {
+    fn new(input: &str) -> Self {
+        let mut data = vec![];
+
+        for line in input.lines() {
+            let row = line.as_bytes().to_vec();
+            data.push(row)
+        }
+
+        let nrows = data.len();
+        let ncols = data[0].len();
+        assert!(data.iter().all(|row| row.len() == ncols));
+
+        Self { nrows, ncols, data }
+    }
+
+    fn at(&self, row: usize, col: usize) -> Option<u8> {
+        self.data.get(row)?.get(col).copied()
+    }
+}
+
+const ADJACENT: &[(isize, isize)] = &[
+    (-1, -1), // top left
+    (-1, 0),  // top
+    (-1, 1),  // top right
+    (0, -1),  // left
+    (0, 1),   // right
+    (1, -1),  // bottom left
+    (1, 0),   // bottom
+    (1, 1),   // bottom right
+];
+fn adjacent_cells(row: usize, col: usize, nrows: usize, ncols: usize) -> Vec<(usize, usize)> {
+    let row: isize = row.try_into().unwrap();
+    let col: isize = col.try_into().unwrap();
+    let mut out = vec![];
+
+    for (drow, dcol) in ADJACENT {
+        let crow = row + drow;
+        if crow < 0 {
+            continue;
+        }
+        let crow = crow as usize;
+        if crow >= nrows {
+            continue;
+        }
+
+        let ccol = col + dcol;
+        if ccol < 0 {
+            continue;
+        }
+        let ccol = ccol as usize;
+        if ccol >= ncols {
+            continue;
+        }
+
+        out.push((crow, ccol));
+    }
+
+    out
+}
+
+fn solve(input: &str) -> u64 {
+    let m = Matrix::new(input);
+    let mut out = 0;
+
+    for row in 0..m.nrows {
+        for col in 0..m.ncols {
+            if m.at(row, col) != Some(b'@') {
+                continue;
+            }
+
+            let mut count = 0;
+            for (arow, acol) in adjacent_cells(row, col, m.nrows, m.ncols) {
+                if let Some(b) = m.at(arow, acol)
+                    && b == b'@'
+                {
+                    count += 1;
+                }
+            }
+
+            if count < 4 {
+                out += 1;
+            }
+        }
+    }
+
+    out
+}
+
+#[test]
+fn test() {
+    assert_eq!(solve(include_str!("input0")), 13)
+}
